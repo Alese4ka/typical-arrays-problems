d573afb8818d6a6fac74de26b9ac5daa65873521
Alese4ka <alesun4ik@gmail.com>
2021-08-20 23:35:41 +0300

    feat: create function min, function max, function avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-
+    if (!array || array.length === 0){
+        return 0;
+    }
+    array.sort((a, b) => b - a);
+    return array[array.length-1];
 }
 
 exports.max = function max (array) {
-
+    if (!array || array.length === 0){
+        return 0;
+    }
+    array.sort((a, b) => b - a);
+    return array[0];
 }
 
 exports.avg = function avg (array) {
-
+    if (!array || array.length === 0){
+        return 0;
+      }
+      let sum = 0; 
+      let avgNum = 1;
+      for (let i = 0; i < array.length; i++){
+        sum = sum + array[i];
+        
+      }
+      avgNum = sum/array.length;
+      return avgNum;
 }
